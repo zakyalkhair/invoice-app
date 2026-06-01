b6c364c48c9c26250c96e8434a4166616d901d6d
KurniaYufi <kurniayufi2005@gmail.com>
2026-06-02 00:05:50 +0700

    fix UI css not rendered

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,23 +7,21 @@ use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         $this->configureDefaults();
+
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 
     protected function configureDefaults(): void
